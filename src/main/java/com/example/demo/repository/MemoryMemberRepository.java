648326b63db83f9973bdf2a6ed593e65be090097
Yooisae <dbdlto@naver.com>
2023-03-07 20:24:24 +0900

    make test code

--- src/main/java/com/example/demo/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/demo/repository/MemoryMemberRepository.java
@@ -28,7 +28,11 @@ public class MemoryMemberRepository implements MemberRepository{
     }
 
     @Override
-    public List<Member> finalAll() {
+    public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
