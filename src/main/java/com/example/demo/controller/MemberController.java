de2299bddcfe3dbf457336e62be25afe0b5fd53f
Yooisae <dbdlto@naver.com>
2023-03-07 23:28:05 +0900

    Configuration for Spring bean

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -9,6 +9,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    //여러가지 방법이 있는데 Constructor 방법이 가장 좋다.
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
