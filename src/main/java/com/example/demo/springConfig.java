5ca80d09369c8cb800bcec2cb6621a61233a805e
Yooisae <dbdlto@naver.com>
2023-03-08 14:25:30 +0900

    jdbc db connection

--- src/main/java/com/example/demo/springConfig.java
+++ src/main/java/com/example/demo/springConfig.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class springConfig {
+
+    private DataSource dataSource;
+    @Autowired
+    public springConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +26,6 @@ public class springConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
